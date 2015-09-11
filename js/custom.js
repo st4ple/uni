17d44e538c28ae2a5d5be16a55fd3588d210a043
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2015-09-11 17:03:11 +0200

    added new semester

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+(function ($) {
+	$( '#dl-menu' ).dlmenu();
+	$('ul.dl-menu li a').smoothScroll();
+
+
+	//animation
+	new WOW().init();
+
+})(jQuery);
+
+(function ($) {
+	$('tbody.time tr td a').smoothScroll();
+
+
+	//animation
+	new WOW().init();
+
+})(jQuery);
